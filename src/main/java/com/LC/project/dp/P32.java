e15952d7ad4a6f741516198c3d9639ebd65b41e0
JiuXiaoY <105855187+JiuXiaoY@users.noreply.github.com>
2024-08-12 16:12:31 +0800

    已完成 95 道题目

--- src/main/java/com/LC/project/dp/P32.java
+++ src/main/java/com/LC/project/dp/P32.java
@@ -7,4 +7,22 @@ package com.LC.project.dp;
  */
 public class P32 {
     // dp 太难了
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (
+                        i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '('
+                )
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+            }
+        }
+        for (int j : dp) {
+            maxans = Math.max(maxans, j);
+        }
+        return maxans;
+    }
 }
---
+++ src/main/java/com/LC/project/dps/P5.java
@@ -0,0 +1,54 @@
+package com.LC.project.dps;
+
+/**
+ * @author JXY
+ * @version 1.0
+ * @since 2024/8/12
+ */
+public class P5 {
+    public static void main(String[] args) {
+
+    }
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        for (int L = 2; L <= len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
